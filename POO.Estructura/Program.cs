95c07f751a1034a46dfd9cdfbf3e0d171cc19182
Jose2San808 <manuelsanpedro.perez08@gmail.com>
2023-05-24 16:24:12 -0500

    Agregar archivos de proyecto.

---
+++ POO.Estructura/Program.cs
@@ -0,0 +1,22 @@
+﻿using POO.Estructura.Enties;
+using POO.Estructura.Service;
+using System;
+
+namespace POO.Estructura
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AlmunoService service = new AlmunoService();
+            Alumnos alumno1 =  service.Agregar();           
+            Console.WriteLine("Hello world");
+            service.Imprimir(alumno1);
+
+            Console.WriteLine("La calificacion es " + service.validarProm(alumno1.Promedio));
+
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ POO.Estructura/Service/AlmunoService.cs
@@ -0,0 +1,59 @@
+﻿using POO.Estructura.Enties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Estructura.Service
+{
+    public class AlmunoService
+    {
+        //responside 
+        public Alumnos Agregar()
+        {
+            try
+            {
+                Alumnos res = new Alumnos()
+                {
+                    Id = 1,
+                    Nombre = "jose ",
+                    Apellido = "san pedro ",
+                    FechaIngreso = DateTime.Now,
+                    Grado = 3,
+                    Promedio = 8,
+                };
+                return res;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Sucede un error " + ex.Message);
+            }
+        }
+
+        public void Imprimir(Alumnos request)
+        {
+            try
+            {
+                Console.WriteLine("Nombre ; "+ request.Nombre);
+                Console.WriteLine("Apellido is; "+ request.Apellido);
+                Console.WriteLine("El dia de ingreso es "+ request.FechaIngreso);
+                Console.WriteLine("Grado "+ request.Grado);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Sucedio un error " + ex.Message);
+            }
+        }
+        public bool validarProm(double num1)
+        {
+            bool validar = false; 
+            if (num1 > 7)
+            {
+                validar = true;
+            }
+            return validar;
+        }
+    }
+}
